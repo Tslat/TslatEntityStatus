ad0dce23a2350250a8a57c52f73439630f8bdbb4
Tslat <tslatmail@gmail.com>
2023-02-23 21:14:26 +1000

    Fix duplicate tick handling and MidnightLib stupid stuff

--- Fabric/src/main/java/net/tslat/tes/TESClient.java
+++ Fabric/src/main/java/net/tslat/tes/TESClient.java
@@ -2,33 +2,20 @@ package net.tslat.tes;
 
 import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.minecraft.client.Minecraft;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.tslat.tes.api.TESConstants;
 import net.tslat.tes.config.TESConfig;
-import net.tslat.tes.core.particle.TESParticleManager;
-import net.tslat.tes.core.state.TESEntityTracking;
 import net.tslat.tes.networking.SyncEffectsPacket;
 
 public class TESClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		ClientTickEvents.END_CLIENT_TICK.register(TESClient::handleClientTick);
 		MidnightConfig.init(TESConstants.MOD_ID, TESConfig.class);
 		TESConstants.setConfig(new TESConfig());
 
 		ClientPlayNetworking.registerGlobalReceiver(SyncEffectsPacket.ID, (client, handler, buf, responseSender) -> SyncEffectsPacket.decode(buf).handleMessage(client::submit));
-	}
-
-	private static void handleClientTick(Minecraft mc) {
-		if (Minecraft.getInstance().level == null)
-			return;
-
-		TESParticleManager.tick();
-		TESEntityTracking.tick();
 	}
 
 	public static void sendPacket(ResourceLocation packetId, FriendlyByteBuf buffer) {
