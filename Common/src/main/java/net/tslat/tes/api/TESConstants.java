426978d536f52f54c4eb3880216cc7f591c43ce4
Tslat <tslatmail@gmail.com>
2023-02-25 11:25:12 +1000

    Update to 1.0.1

--- Common/src/main/java/net/tslat/tes/api/TESConstants.java
+++ Common/src/main/java/net/tslat/tes/api/TESConstants.java
@@ -11,7 +11,7 @@ import java.util.ServiceLoader;
  * Common class to store all of the globally & statically accessible data points for TES
  */
 public final class TESConstants {
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final String MOD_ID = "tslatentitystatus";
 	public static final String MOD_NAME = "TES";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
