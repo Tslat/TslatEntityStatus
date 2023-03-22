e3b9487b02706092968b791de30db67229d88159
Tslat <tslatmail@gmail.com>
2023-03-22 16:04:40 +1000

    Document the existing networking hooks

--- Common/src/main/java/net/tslat/tes/core/networking/TESNetworking.java
+++ Common/src/main/java/net/tslat/tes/core/networking/TESNetworking.java
@@ -11,7 +11,28 @@ import java.util.Set;
  * Access this from {@link net.tslat.tes.api.TESConstants#NETWORKING TESConstants.NETWORKING}
  */
 public interface TESNetworking {
+	/**
+	 * Request an update for {@link net.minecraft.world.effect.MobEffect MobEffects} for a given entity<br>
+	 * Network direction: (CLIENT -> SERVER)
+	 */
 	void requestEffectsSync(int entityId);
+
+	/**
+	 * Send an update for {@link net.minecraft.world.effect.MobEffect MobEffects} for a given entity to a specific player (usually in response to a {@link TESNetworking#requestEffectsSync prior request}<br>
+	 * Network direction: SERVER -> CLIENT
+	 * @param player The player to send to
+	 * @param entityId The id of the entity to update
+	 * @param toAdd The effects to add to the entity's state on the client side (usually all of them)
+	 * @param toRemove The effects to remove from the entity's state on the client side (usually empty)
+	 */
 	void sendEffectsSync(ServerPlayer player, int entityId, Set<ResourceLocation> toAdd, Set<ResourceLocation> toRemove);
+
+	/**
+	 * Send an update for {@link net.minecraft.world.effect.MobEffect MobEffects} to all players tracking the given entity. Usually as part of an effect being added/removed<br>
+	 * Network direction: SERVER -> CLIENT
+	 * @param targetedEntity The entity to update for
+	 * @param toAdd The effects to add to the entity's state on the client side
+	 * @param toRemove The effects to remove from the entity's state on the client side
+	 */
 	void sendEffectsSync(LivingEntity targetedEntity, Set<ResourceLocation> toAdd, Set<ResourceLocation> toRemove);
 }
