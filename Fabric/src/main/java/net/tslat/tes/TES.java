3c8b5bf24385860c765b96210edabd9245841ef3
Tslat <tslatmail@gmail.com>
2023-04-04 15:17:09 +1000

    Fix effects not working properly on Fabric

--- Fabric/src/main/java/net/tslat/tes/TES.java
+++ Fabric/src/main/java/net/tslat/tes/TES.java
@@ -2,13 +2,17 @@ package net.tslat.tes;
 
 import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.tslat.tes.api.TESConstants;
 import net.tslat.tes.config.TESConfig;
+import net.tslat.tes.networking.RequestEffectsPacket;
 
 public class TES implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		MidnightConfig.init(TESConstants.MOD_ID, TESConfig.class);
 		TESConstants.setConfig(new TESConfig());
+
+		ServerPlayNetworking.registerGlobalReceiver(RequestEffectsPacket.ID, (server, player, handler, buf, responseSender) -> RequestEffectsPacket.decode(buf).handleMessage(player, server::submit));
 	}
 }
