643a32e30f72658ada69c8e97f881e190e3ac41f
Tslat <tslatmail@gmail.com>
2023-08-13 13:44:42 +1000

    Clear tag-based caches on server start to prevent cross-server tags being handled incorrectly

--- Fabric/src/main/java/net/tslat/tes/TES.java
+++ Fabric/src/main/java/net/tslat/tes/TES.java
@@ -2,6 +2,7 @@ package net.tslat.tes;
 
 import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.tslat.tes.api.TESConstants;
 import net.tslat.tes.config.TESConfig;
 
@@ -10,5 +11,6 @@ public class TES implements ModInitializer {
 	public void onInitialize() {
 		MidnightConfig.init(TESConstants.MOD_ID, TESConfig.class);
 		TESConstants.setConfig(new TESConfig());
+		ServerLifecycleEvents.SERVER_STARTED.register(server -> TESConstants.UTILS.clearDynamicCaches());
 	}
 }
