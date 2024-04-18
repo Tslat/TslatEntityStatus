f6764b30bcfe80e32f59dc94562ffc7e3b4bfa84
Tslat <tslatmail@gmail.com>
2024-04-18 15:50:58 +1000

    Fix effects request not doing anything on Fabric

--- Fabric/src/main/java/net/tslat/tes/TES.java
+++ Fabric/src/main/java/net/tslat/tes/TES.java
@@ -2,11 +2,14 @@ package net.tslat.tes;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.tslat.tes.api.TESConstants;
+import net.tslat.tes.networking.RequestEffectsPacket;
 
 public class TES implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ServerLifecycleEvents.SERVER_STARTED.register(server -> TESConstants.UTILS.clearDynamicCaches());
+		ServerPlayNetworking.registerGlobalReceiver(RequestEffectsPacket.ID, (server, player, handler, buf, responseSender) -> RequestEffectsPacket.decode(buf).handleMessage(player, server::submit));
 	}
 }
