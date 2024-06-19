2490cea047ce8b31834195b001188f2bb7fdb476
Tslat <tslatmail@gmail.com>
2024-06-19 17:53:31 +1000

    Cleanup

--- Common/src/main/java/net/tslat/tes/api/TESConstants.java
+++ Common/src/main/java/net/tslat/tes/api/TESConstants.java
@@ -8,7 +8,6 @@ import net.tslat.tes.api.util.TESUtil;
 import net.tslat.tes.core.networking.TESNetworking;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ServiceLoader;
 
@@ -16,11 +15,10 @@ import java.util.ServiceLoader;
  * Common class to store all of the globally and statically accessible data points for TES
  */
 public final class TESConstants {
-	public static final String VERSION = "1.4.5";
+	public static final String VERSION = "1.5.5";
 	public static final String MOD_ID = "tslatentitystatus";
 	public static final String MOD_NAME = "TES";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
-	@Nullable
 	public static TESConfig CONFIG = null;
 
 	public static TESUtil UTILS = ServiceLoader.load(TESUtil.class).findFirst().get();
