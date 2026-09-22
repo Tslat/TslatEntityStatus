40b925f62837dd11c80d04f9cd437a5a74ad0bd0
Tslat <7098525+Tslat@users.noreply.github.com>
2026-09-22 18:14:59 +1000

    Fix TESstate being bound on server side

--- Common/src/main/java/net/tslat/tslatentitystatus/mixin/client/EntityClientMixin.java
+++ Common/src/main/java/net/tslat/tslatentitystatus/mixin/client/EntityClientMixin.java
@@ -2,17 +2,23 @@ package net.tslat.tslatentitystatus.mixin.client;
 
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
 import net.tslat.tslatentitystatus.core.state.TESEntityState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
 public class EntityClientMixin {
+    @Shadow
+    private Level level;
+
     @Inject(method = "setId", at = @At("TAIL"))
     private void tslatentitystatus$captureLocalPlayer(int id, CallbackInfo ci) {
-        if ((Object)this instanceof LivingEntity livingEntity)
+        //noinspection ConstantValue
+        if ((Object)this instanceof LivingEntity livingEntity && level.isClientSide())
             livingEntity.tslatentitystatus$setEntityState(new TESEntityState(livingEntity));
     }
 }
